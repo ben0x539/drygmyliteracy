fea6fa1e4d886829a1bc2a996a39b9e16a0561a9
Benjamin Herr <git@1d6.org>
2026-02-14 05:47:21 -0800

    BookWriter: hard wrap at 19 chars
    A line fits 19 'm's with the default font, so this is a lower bound for
    valid line length.

--- src/main/java/org/_1d6/drygmyliteracy/BookWriter.java
+++ src/main/java/org/_1d6/drygmyliteracy/BookWriter.java
@@ -9,6 +9,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookWriter {
+    // I don't know what the fuck we're supposed to be doing with filterable
+    // strings, so, uh, let's just leave existing pages completely untouched
+    // and track a new, in-progress page using regular strings and then
+    // send that into a passthrough filterable?
+    //
+    // We maintain the invariant of pages having 100 or less elements, and
+    // additionally keep newPage null if it is at 100 elements. newPage of
+    // course has to be under 14 elements.
+    //
+    // TODO: the hard part is going to be keeping each line in newPage from
+    // being long enough to need to be wrapped, because then we lose track
+    // of how many real, wrapped lines newPage is going to take up, and we
+    // will easily overrun the end of the page, visually.
     private final List<Filterable<String>> pages;
     private List<String> newPage;
 
@@ -36,12 +49,25 @@ public class BookWriter {
     }
 
     public boolean writeLine(String line) {
-        if (newPage == null || newPage.size() >= 14) {
-            if (!maybePrepareNewPage())
-                return false;
-        }
+        while (!line.isEmpty()) {
+            // TODO: hard-wrapping at 19 looks pretty bad, but we don't have
+            // font metrics on the server side. So unless we want to sort of
+            // measure the default font by hand and calculate the exact
+            // string with ourselves there isn't really a good thing to do
+            // here.
+            // TODO: Don't break in the middle of words or before punctuation,
+            // I guess.
+            int n = Math.min(19, line.length());
+            String prefix = line.substring(0, n);
+            line = line.substring(n);
 
-        newPage.add(line);
+            if (newPage == null || newPage.size() >= 14) {
+                if (!maybePrepareNewPage())
+                    return false;
+            }
+
+            newPage.add(prefix);
+        }
         return true;
     }
 
