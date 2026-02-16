3bbb2686d41077c3da8ca6d907a951528adb1628
Benjamin Herr <git@1d6.org>
2026-02-15 20:31:55 -0800

    BookWriter: try to take char width into account for wrapping
    took a bunch of widths from a gist i found on google:
    https://gist.github.com/aerulion/9c5aac21c3efa5aa03b6f875dbe808de
    
    doesn't handle surrogate pairs right or anything. welp.

---
+++ src/test/java/org/_1d6/drygmyliteracy/BookWriterTest.java
@@ -0,0 +1,10 @@
+package org._1d6.drygmyliteracy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookWriterTest {
+
+    @org.junit.jupiter.api.Test
+    void getWrapPoint() {
+    }
+}
