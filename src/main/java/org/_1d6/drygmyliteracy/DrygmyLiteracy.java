e3d3ffc67f5606a69c96bebf0835381be56185d2
Benjamin Herr <git@1d6.org>
2026-02-18 12:33:40 -0800

    delete some stuff from the example repo we don't need

--- src/main/java/org/_1d6/drygmyliteracy/DrygmyLiteracy.java
+++ src/main/java/org/_1d6/drygmyliteracy/DrygmyLiteracy.java
@@ -39,11 +39,5 @@ public class DrygmyLiteracy {
     // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
-    public DrygmyLiteracy(IEventBus modEventBus, ModContainer modContainer) {
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-
-    }
+    public DrygmyLiteracy(IEventBus modEventBus, ModContainer modContainer) {}
 }
